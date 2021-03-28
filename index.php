c74d36ce190b3003e27ab8bb988f968eb6247fbb
Сергей Диденко <s.didenko@rabota.ru>
2021-03-28 18:50:53 +0300

    добавил наложение лого

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ function createImage(): string
     $imageLink = __DIR__ . '/img/banner.jpg';
     $font = __DIR__ . "/font/ptsans.ttf";
     $imageCoverLink = 'https://img2.pngio.com/transparent-grey-background-png-6-png-image-grey-background-png-1920_1080.png';
+    $logoLink = __DIR__ . '/img/logo.png';
     $newImageName = __DIR__  . '/tmp/' . generateRandomName() . '.png';
 
     $title = 'Пользуясь случаем';
@@ -21,14 +22,26 @@ function createImage(): string
 
     // получаем изображение
     $image = imagecreatefromjpeg($imageLink);
-    imagecrop($image, ['x' => 0, 'y' => 0, 'width' => 1200, 'height' => 600]);
+
+    //получаем фильтр для затемнения
     $imageCover = imagecreatefrompng($imageCoverLink);
 
+    //получем лого
+    $logo = imagecreatefrompng($logoLink);
+
+    //накладываем затемнение
     imagealphablending($imageCover, true);
     imagesavealpha($imageCover, true);
     imagealphablending($image, true);
     imagesavealpha($image, true);
     imagecopy($image, $imageCover, 0, 0, 0, 0, imagesx($imageCover), imagesy($imageCover));
+
+    //накладываем лого
+    imagealphablending($logo, true);
+    imagesavealpha($logo, true);
+    imagealphablending($image, true);
+    imagesavealpha($image, true);
+    imagecopy($image, $logo, 44, 44, 0, 0, imagesx($logo), imagesy($logo));
 
     //создаем цвет надписи
     $color = imagecolorallocate($image, 0xFF, 0xFF, 0xFF);
